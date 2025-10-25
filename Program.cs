3b3b60ab008bb8082e08c3a7a4df675f3c72e38f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-10-24 22:29:37 -0300

    Atividade pronta

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿double peso;
+double altura;
+double imc;
+
+
+string diagnostico;
+
+
+Console.WriteLine("-- Calculadora de IMC --\n");
+
+
+Console.Write("Digite seu peso em kg...: ");
+peso = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Digite sua altura em m..: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+
+imc = peso / Math.Pow(altura, 2);
+//ou
+//imc = peso / (altura * altura);
+
+
+if (imc < 17)
+{
+    diagnostico = "Muito abaixo do peso";
+}
+else if (imc < 18.5)
+{
+    diagnostico = "Abaixo do peso";
+}
+else if (imc < 25)
+{
+    diagnostico = "Peso normal";
+}
+else if (imc < 30)
+{
+    diagnostico = "Acima do peso";
+}
+else if (imc < 35)
+{
+    diagnostico = "Obesidade I";
+}
+else if (imc < 40)
+{
+    diagnostico = "Obesidade II(severa)";
+}
+else 
+{
+    diagnostico = "Obesidade III (morbida)";
+}
+
+Console.WriteLine($"\nSeu IMC é {imc:N2} kg/m².");
+Console.WriteLine($"Diagnóstico: {diagnostico}");
